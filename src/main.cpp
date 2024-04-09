50bd97dd15744067144ce021634c5255ac17e715
Marius Gundersen <marius.gundersen@clave.no>
2024-04-09 15:49:00 +0200

    print wakeup reason

--- src/main.cpp
+++ src/main.cpp
@@ -95,6 +95,35 @@ int drawImg(JPEGDRAW *pDraw)
   return 1;
 }
 
+void print_wakeup_reason()
+{
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+  case ESP_SLEEP_WAKEUP_EXT0:
+    Serial.println("Wakeup caused by external signal using RTC_IO");
+    break;
+  case ESP_SLEEP_WAKEUP_EXT1:
+    Serial.println("Wakeup caused by external signal using RTC_CNTL");
+    break;
+  case ESP_SLEEP_WAKEUP_TIMER:
+    Serial.println("Wakeup caused by timer");
+    break;
+  case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    Serial.println("Wakeup caused by touchpad");
+    break;
+  case ESP_SLEEP_WAKEUP_ULP:
+    Serial.println("Wakeup caused by ULP program");
+    break;
+  default:
+    Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+    break;
+  }
+}
+
 void setup()
 {
   pinMode(BUSY_Pin, INPUT);
@@ -107,6 +136,8 @@ void setup()
   Serial.begin(115200);
 
   delay(5000);
+
+  print_wakeup_reason();
 
   wm.autoConnect("e-ink", "password");
 
