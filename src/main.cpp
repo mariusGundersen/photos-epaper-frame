f6e5a51063ef3a10610f4511ecff31e804b9707b
Marius Gundersen <marius.gundersen@clave.no>
2024-10-15 09:50:03 +0200

    Got the displays to work

---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <EPD_7in3e.h>
+#include <Adafruit_ST7789.h>
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, -1);
+uint8_t x = 0;
+uint16_t rgb = 0;
+GFXcanvas8 epd = GFXcanvas8(EPD_7IN3E_WIDTH, EPD_7IN3E_HEIGHT);
+
+void setup()
+{
+  Serial.begin();
+  delay(1000);
+  Serial.println("Started");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+  tft.init(135, 240);
+
+  delay(500);
+
+  tft.enableDisplay(true);
+
+  tft.fillRect(0, 0, 135, 240, ST77XX_BLUE);
+
+  Serial.println(("TFT INIT"));
+
+  tft.drawLine(70, 0, 70, 239, ST77XX_GREEN);
+
+  Debug("EPD_7IN3E_test Demo\r\n");
+  if (DEV_Module_Init() != 0)
+  {
+    return;
+  }
+
+  Debug("e-Paper Init and Clear...\r\n");
+  EPD_7IN3E_Init();
+  EPD_7IN3E_Clear(EPD_7IN3E_WHITE); // WHITE
+  DEV_Delay_ms(1000);
+
+  epd.fillScreen(EPD_7IN3E_BLACK);
+  /*  epd.drawRect(0, 0, 133, EPD_7IN3E_HEIGHT, EPD_7IN3E_BLACK);
+    epd.drawRect(0, 133, 133, EPD_7IN3E_HEIGHT, EPD_7IN3E_BLUE);
+    epd.drawRect(0, 266, 134, EPD_7IN3E_HEIGHT, EPD_7IN3E_RED);
+    epd.drawRect(0, 400, 133, EPD_7IN3E_HEIGHT, EPD_7IN3E_GREEN);
+    epd.drawRect(0, 533, 133, EPD_7IN3E_HEIGHT, EPD_7IN3E_YELLOW);
+    epd.drawRect(0, 666, 134, EPD_7IN3E_HEIGHT, EPD_7IN3E_WHITE);
+  */
+  // EPD_7IN3E_Display(epd.getBuffer());
+  EPD_7IN3E_Show();
+
+  EPD_7IN3E_Sleep();
+
+  SPI.endTransaction();
+
+  tft.enableDisplay(true);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+
+  tft.drawLine(x, 0, x, 239, rgb++);
+
+  x = (x + 1) % 135;
+
+  Serial.println("Blink");
+
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
